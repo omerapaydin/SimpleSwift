0f4eea6aeee051f9aa7b681afbcd733d7ea34b1f
Omer Apaydin <omerapaydin90@gmail.com>
2024-03-02 15:45:59 +0300

    Initial Commit

---
+++ DenemeX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DenemeX
+//
+//  Created by Ömer on 2.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
