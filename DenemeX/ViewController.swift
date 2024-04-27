3d2270c08bf96c27e6a6edc6be3ada89fd45ac26
Omer Apaydin <omerapaydin90@gmail.com>
2024-04-27 19:27:51 +0300

    Second

--- DenemeX/ViewController.swift
+++ DenemeX/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var userName = ""
 
+    @IBOutlet weak var passLabel2: UITextField!
+    @IBOutlet weak var passLabel: UITextField!
+    @IBOutlet weak var nameLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
+    @IBAction func giris(_ sender: Any) {
+        
+        if nameLabel.text == "" {
+            makeAlert(inputTitle: "Hatalı", inputMessage: "İsim gir")
+            
+        }else if passLabel.text == "" {
+            makeAlert(inputTitle: "Hatalı", inputMessage: "Şifre gir")
+
+            
+        }else if passLabel.text != passLabel2.text {
+            makeAlert(inputTitle: "Hatalı", inputMessage: "Şifre eşleşmiyor")
+
+            
+        }else {
+            
+            userName = nameLabel.text!
+            performSegue(withIdentifier: "goTo2", sender: nil)
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goTo2" {
+            let des = segue.destination as! ViewController2
+            des.names = userName
+        }
+    }
+    
+    
+    
+    
+    func makeAlert (inputTitle : String , inputMessage : String) {
+        let alert = UIAlertController(title: inputTitle , message: inputMessage , preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
+        
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+    
+    
+    
 }
 
--- DenemeX/ViewController2.swift
+++ DenemeX/ViewController2.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class ViewController2: UIViewController {
+    
+    var timer = Timer()
+    var counter = 10
+    var names = ""
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        textLabel.text = names
+        
+        imageView.isUserInteractionEnabled = true
+        let gest = UITapGestureRecognizer(target: self, action: #selector(reImage))
+        imageView.addGestureRecognizer(gest)
+        
+        timeLabel.text = "Time : \(counter)"
+        counter = 10
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timeT), userInfo: nil, repeats: true)
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @objc func timeT(){
+        
+        timeLabel.text = "Time : \(counter)"
+        counter -= 1
+        if counter == 0 {
+            timer.invalidate()
+            timeLabel.text = "Time is over  !"
+        }
+        
     }
-    */
+    
+    @objc func reImage(){
+        
+        imageView.image = UIImage(named: "tony")
+        
+    }
+    
 
 }
